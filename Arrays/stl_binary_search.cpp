965de19d6dd54824d7924247947af08e05e3aef5
Unceas <kayush2005k@gmail.com>
2026-01-29 11:28:55 +0530

    Added STL-based binary search and updated README

---
+++ Arrays/stl_binary_search.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    vector<int> arr = {5, 10, 15, 20, 25};
+
+    int key = 15;
+
+    if (binary_search(arr.begin(), arr.end(), key))
+        cout << "Element found using STL";
+    else
+        cout << "Element not found";
+
+    return 0;
+}
+
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
