a86dfc3cdd397fc32e3633bd6f39c9b3a0939e7b
Unceas <kayush2005k@gmail.com>
2026-02-12 00:32:02 +0530

    Added linked list insertion at end

---
+++ LinkedList/insert_at_end.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+// Insert at end
+void insertAtEnd(Node*& head, int val) {
+    Node* newNode = new Node(val);
+
+    if (head == NULL) {
+        head = newNode;
+        return;
+    }
+
+    Node* temp = head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = newNode;
+}
+
+// Print list
+void printList(Node* head) {
+    while (head != NULL) {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL";
+}
+
+int main() {
+    Node* head = NULL;
+
+    insertAtEnd(head, 10);
+    insertAtEnd(head, 20);
+    insertAtEnd(head, 30);
+
+    printList(head);
+
+    return 0;
+}
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
